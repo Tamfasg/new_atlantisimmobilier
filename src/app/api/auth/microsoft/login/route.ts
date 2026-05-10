7419d1db3f65f393fd8c42e1fca7165f0dd6b921
Tamfasg <tarikfquihi16@gmail.com>
2026-05-10 20:24:45 +0100

    protect url with custome key

--- src/app/api/auth/microsoft/login/route.ts
+++ src/app/api/auth/microsoft/login/route.ts
@@ -1,9 +1,19 @@
-import { NextResponse } from "next/server";
+// src/app/api/auth/microsoft/login/route.ts
+import { NextRequest, NextResponse } from "next/server";
 import { microsoftScopes, msalClient } from "@/lib/microsoft/msal";
 
 export const runtime = "nodejs";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const key = req.nextUrl.searchParams.get("key");
+
+  if (key !== process.env.MICROSOFT_ADMIN_CONNECT_KEY) {
+    return NextResponse.json(
+      { success: false, error: "Unauthorized" },
+      { status: 401 },
+    );
+  }
+
   const authUrl = await msalClient.getAuthCodeUrl({
     scopes: microsoftScopes,
     redirectUri: process.env.MICROSOFT_REDIRECT_URI!,
